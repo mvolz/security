9b22f75c81380e134e44af26d31ef6d4d862f98b
Marielle Volz <marielle.volz@gmail.com>
2014-10-08 07:48:34 +0100

    initial commit

---
+++ static/How to Encrypt Credit Card Information in an SQL Database   eHow_files/community.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 21:19:35 Sep 21, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:26:13 Oct 4, 2014.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+(function(){if(typeof window.Community!="undefined"){return}window.Community={debug:false,global:{},user:{id:null}}})();Community.config={env:"production",base_url:"/web/20140921211935/https://community.ehow.com",api:{path:"/api/v1",discussion:"/api/v1/community_discussion"}};$(function(){var b=false;function a(){var d=document.createElement("script");d.type="text/javascript";d.async=true;d.src=Community.config.base_url+"/widget/ehow/communityload.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(d,c);b=true}if($(window).scrollTop()>500&&b==false){a()}$(window).scroll(function(){if($(window).scrollTop()>500&&b==false){a()}})});
---
+++ static/How to Encrypt Credit Card Information in an SQL Database   eHow_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "ILFORD",
+      'continent': "EU",
+      'country': "GB",
+      'region': "EN"
+    },
+    'ip':"86.128.222.38"
+  }]);
+})
+//
+()
+
+;
